64ab5a67a2f995923ff7743c3db84b9ae6aed2fa
Kosalsensok <kosalsensok@gmail.com>
2026-08-27 14:32:48 +0700

    fix(ui): remove crisp-edges pixelation rule and apply silky-smooth anti-aliased Lanczos HD logo rendering

--- public/build/assets/Index-BIakHx8w.js
+++
@@ -1,1 +0,0 @@
-import{F as e,d as t,x as n}from"./vendor-ui-BDZn_Bea.js";import{t as r}from"./Index-D0B-OYLX.js";var i=n({__name:`Index`,props:{enrollments:{},activeTab:{}},setup(n){return(i,a)=>(e(),t(r,{enrollments:n.enrollments,activeTab:n.activeTab},null,8,[`enrollments`,`activeTab`]))}});export{i as default};
